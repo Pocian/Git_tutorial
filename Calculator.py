bb97200013d2bfbf3f80d62967547f9e4c06017d
Posiano Francis <Pocian98@gmail.com>
2025-07-04 16:04:30 +0300

    Calculator Version 3

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,25 @@
-Num1 = float(input("Enter first number: "))
-Num2 = float(input("Enter second number: "))
-Operation = input("Enter operation (+, -, *, /): ")
+def calculator():
 
-if Operation == "+":
+  Num1 = float(input("Enter first number: "))
+  Num2 = float(input("Enter second number: "))
+  Operation = input("Enter operation (+, -, *, /): ")
+
+  if Operation == "+":
     Result = Num1 + Num2
-elif Operation == "-":
+  elif Operation == "-":
     Result = Num1 - Num2
-elif Operation == "*":
+  elif Operation == "*":
     Result = Num1 * Num2
-elif Operation == "/":
+  elif Operation == "/":
     Result = Num1 / Num2
-else:
+  else:
     Result = "Invalid operation"
 
-print("Result:", Result)
+  print("Result:", Result)
+
+while True:
+  calculator()
+  cont = input("Do you want to perform another calculation? (yes/no): ")
+  if cont.lower() != 'yes':
+    break
+print("Thank you for using the calculator!")
