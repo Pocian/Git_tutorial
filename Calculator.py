a29dd83663849b20f512176a8683697dc3eb0385
Posiano Francis <Pocian98@gmail.com>
2025-07-04 15:26:03 +0300

    Calculator Version 2

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,16 @@
 Num1 = float(input("Enter first number: "))
 Num2 = float(input("Enter second number: "))
 Operation = input("Enter operation (+, -, *, /): ")
+
+if Operation == "+":
+    Result = Num1 + Num2
+elif Operation == "-":
+    Result = Num1 - Num2
+elif Operation == "*":
+    Result = Num1 * Num2
+elif Operation == "/":
+    Result = Num1 / Num2
+else:
+    Result = "Invalid operation"
+
+print("Result:", Result)
